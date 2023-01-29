a8c15056e7daaa4efb96c2852f32e5dce6949964
Krishnan Winter <krishnanwinter1@gmail.com>
2023-01-29 19:18:57 +1100

    pushed dtb addr onto stack: :

--- core/boot.s
+++ core/boot.s
@@ -2,5 +2,6 @@
 _start:
     ldr x30, =stack_top
     mov sp, x30
+    str     x2, [sp, #4]
     bl main
     b .
--- core/main.c
+++ core/main.c
@@ -1,6 +1,12 @@
 #include <stdint.h>
 #include <stdio.h>
  
-void main(void) {
+void main(uint32_t dtb_pa) {
     printk("Hello world!\n");
+    uint32_t *temp = (uint32_t *) dtb_pa;
+    printk("%x\n", *temp);
+    for (int i = 0; i < 4; i++) {
+        printk("%x\n", *((uint8_t *)temp + i));
+    }
+
 }
